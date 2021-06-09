bb4536c5f2718d8f54f85404933149f72cb5d5d7
Jacob Scase <jacobscase@hotmail.com>
2021-06-09 14:42:43 +0100

    Initial Commit

---
+++ SwiftUISlots/SwiftUISlotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISlotsApp.swift
+//  SwiftUISlots
+//
+//  Created by Jacob Scase on 09/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISlotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
