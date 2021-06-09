7cbb3f7998980ab1768952f27e0a380c99cfb35e
Jacob Scase <jacobscase@hotmail.com>
2021-06-09 14:56:01 +0100

    initial commit, laying out items

--- SwiftUISlots/ContentView.swift
+++ SwiftUISlots/ContentView.swift
@@ -8,9 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var credits: Int = 0
+    @State var slot1Img: String = "apple"
+    @State var slot2Img: String = "cherry"
+    @State var slot3Img: String = "star"
     var body: some View {
-        Text("Hello, world!")
+        VStack{
+            Text("Swift UI Slots")
+                .padding()
+                .font(/*@START_MENU_TOKEN@*/.title/*@END_MENU_TOKEN@*/)
+            Text("Credits: \(credits)")
+                .padding()
+            HStack{
+                Image(slot1Img)
+                Image(slot2Img)
+                Image(slot3Img)
+            }
             .padding()
+            Button("Spin") {
+                print("Hello world")
+            }
+            .padding()
+        }
     }
 }
 
